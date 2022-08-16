2e95a8c5093303050f59b5b9f08b4a2054877357
mohammadrezakarimi <k.reza90@yahoo.com>
2022-08-16 23:13:19 +0430

    Update

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from b4 import *
+from bs4 import BeautifulSoup
 import requests
+
+# geeksforgeeks python history url
+url = requests.get('https://www.geeksforgeeks.org/history-of-python/')
+
+soup = BeautifulSoup(url.content, 'lxml')
+
+#print(soup.title.text)
+
+s = soup.find_all('div', class_='text')
+
+for text in s:
+    t = text.text
+
+with open('history.html', 'w',encoding='utf-8') as file:
+    file.write(str(t))
